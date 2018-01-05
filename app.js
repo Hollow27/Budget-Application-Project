da18f9ced0a843132c64d54f11764c03d8a4515a
Zohaib Shahzad <zohaibshahzad.to@gmail.com>
2018-01-05 01:07:25 -0500

    UI Controller Work
    Created an AddListItem Function where we add placeholders and allow the user to actually input new information to the UI

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ var DOMstrings = {
     inputDescription: '.add__description',
     inputValue: '.add__value',
     inputBtn: '.add__btn'
+    incomeContainer: '.income__list',
+    expenseContainer: '.expenses__list'
 
 };
 
@@ -55,6 +57,29 @@ var DOMstrings = {
        getDOMstrings. All we need in this function in this method here is to return our private DOMstrings into
        public scope.
     */
+
+    addListItem: function(obj, type) {
+      var html, newHtml, element;
+      // Create HTML string with placeholder text
+
+      if(type === 'inc') {
+      element = DOMstrings.incomeContainer;
+      html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">&value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      } else if(type === 'exp') {
+      element = DOMstrings.expenseContainer;
+      html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+      }
+
+      // Replace the placeholder text with some actual data
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+      //Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+    },
+
     getDOMstrings: function() {
         return DOMstrings;
     }
